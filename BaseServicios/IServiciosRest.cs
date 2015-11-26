3c1b45657a9749c3554cb18336a5ad5b0a1413b5
luis@yo.es <luis@yo.es>
2015-11-26 14:24:15 +0100

    ConsumoApiRest2015

---
+++ BaseServicios/IServiciosRest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseServicios
+{
+   public interface IServiciosRest<TModelo>
+   {
+       Task<TModelo> Add(TModelo model);
+       Task Update(TModelo model);
+       Task Delete(TModelo model);
+
+       List<TModelo> Get();
+       List<TModelo> Get(Dictionary<String, String> param);
+       TModelo Get(int id);
+
+   }
+}
---
+++ BaseServicios/ServicioRestImpl.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace BaseServicios
+{
+   public class ServicioRestImpl<TModelo>:IServiciosRest<TModelo>
+   {
+       private String url;
+       private bool auth;
+       private String user;
+       private String pass;
+
+       public ServicioRestImpl(String url, bool auth = false, String user = null, String pass = null)
+       {
+           this.url = url;
+           this.auth = auth;
+           this.user = user;
+           this.pass = pass;
+       }
+
+
+       public Task<TModelo> Add(TModelo model)
+       {
+           throw new NotImplementedException();
+       }
+
+       public Task Update(TModelo model)
+       {
+           throw new NotImplementedException();
+       }
+
+       public Task Delete(TModelo model)
+       {
+           throw new NotImplementedException();
+       }
+
+       public List<TModelo> Get()
+       {
+           throw new NotImplementedException();
+       }
+
+       public List<TModelo> Get(Dictionary<string, string> param)
+       {
+           throw new NotImplementedException();
+       }
+
+       public TModelo Get(int id)
+       {
+           throw new NotImplementedException();
+       }
+    }
+}
