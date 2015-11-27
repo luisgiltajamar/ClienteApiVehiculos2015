d6bd71b94e234df5301b1f3117171384b5a1b03c
luis@yo.es <luis@yo.es>
2015-11-27 12:47:34 +0100

    This is the end

--- ClienteApiVehiculos/Controllers/TipoController.cs
+++ ClienteApiVehiculos/Controllers/TipoController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using BaseServicios;
@@ -18,6 +19,19 @@ namespace ClienteApiVehiculos.Controllers
         {
             var data = Servicio.Get();
             return View(data);
+        }
+
+        public ActionResult Alta()
+        {
+            return View(new TipoVehiculo());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Alta(TipoVehiculo model)
+        {
+            var data = await Servicio.Add(model);
+
+            return RedirectToAction("Index");
         }
     }
 }
